68f694e9c304f1a750867e6c35c4beb8b6b0173a
Ann-Mamaeva <64729127+Ann-Mamaeva@users.noreply.github.com>
2020-06-03 22:32:53 +0300

    Add files via upload

---
+++ re.email.py
@@ -0,0 +1,14 @@
+#сделать программу, которая собирает данные пользователей
+import re
+filename = 'program.txt'
+with open(filename, 'w') as file:
+    file.write('')
+while True:
+    print('Укажите почту')
+    email = input()
+    a = '\w+@\w+.\w+'  #регулярное выражение, что совпадает с почтой вида test@gmail.com
+    if re.match(r'\w+@\w+.\w+',email):
+        with open(filename, 'a') as file:
+            file.write(str(email)+'\n')
+    else:
+        print('Введите почту вида: test@gmail.com')
